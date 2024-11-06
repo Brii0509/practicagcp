ca72170e0bc50402f3c1cbaa1f7b77418cb64757
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 11:44:07 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Estas son las tendencias tecnológicas centradas en la nube para el 2024, Perú",
+"autor" => "diario el peruano",
+"resumen" => "El product manager de cloud y data center de Internexa, Jorge Gómez, declaró: Anuncios como los de AWS de incursionar en nuestro país movilizan de forma inmediata la incorporación de nuevos jugadores en la industria, así como de un crecimiento a desarrollar en los próximos años en términos de Datacenter en modelo hiperescala ",
 ];
 ?>
