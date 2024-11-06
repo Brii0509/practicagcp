906043f890b67e8413ca4ccc98306ccf3428d9e6
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 11:48:15 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Siete tendencias de una estrategia empresarial en la nube para 2024",
+"autor" => "John Edwards",
+"resumen" => "La nube ha evolucionado hasta convertirse en el sistema operativo de la empresa del futuro, trascendiendo su papel inicial como medida de ahorro de costes",
 ];
 ?>
