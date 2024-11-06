b4aeb1d62a83850a59368ad901ff2d72fc525b03
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 12:00:34 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Aplicaciones de computación en la nube para la ciencia biomédica",
+"autor" => "Graciela Riera",
+"resumen" => "La investigación biomédica se ha convertido en un esfuerzo intensivo basado en una infraestructura de computación, almacenamiento y de red segura y escalable, que tradicionalmente se ha adquirido, apoyado y mantenido de forma local. Para ciertos tipos de aplicaciones biomédicas, el “Cloud Computing” o computación en la nube ha surgido como una alternativa a los enfoques de computación tradicionales mantenidos localmente.",
 ];
 ?>
