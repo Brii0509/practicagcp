ec5c1bb994a4d4926b3ad2c2e5c542b905d79ed6
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 11:40:34 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Siete beneficios de la Nube que potencian la transformación empresarial",
+"autor" => "telefonica",
+"resumen" => "Según el estudio “Cloud en América Latina 2023”, el 98% de las empresas de la región ya iniciaron su camino hacia la nube, mientras que un 80% se encuentra en etapa de implementación.
+Movistar Empresas ofrece conectividad estable y segura, junto con una amplia gama de soluciones digitales, como servicios en la nube, para impulsar la transformación de empresas grandes y pymes.",
 ];
 ?>
