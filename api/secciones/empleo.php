7bd2a252f8957ecd8cf533f83396c47fd7f80666
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 11:50:44 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Las 4 mejores aplicaciones en la nube gratis",
+"autor" => "Dr. Frederic Llordachs Marqués",
+"resumen" => "La aplicaciones en la nube son los son los servicios basados ​​en la nube también conocidos como software como servicio (SaaS, por sus siglas en inglés). Muchos de los programas que una vez tuvieron que ser instalados en los equipos de forma individual ahora se ofrecen en línea, y lo único que se necesita para acceder al programa es una cuenta y una contraseña. Vamos a mostrarte las 4 mejores aplicaciones gratis en la nube.",
 ];
 ?>
