a7742f901003b03b9b950cdbe08b13a36060e21c
Brii0509 <chatoandpeque@gmail.com>
2024-11-06 11:58:50 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Dropbox",
+"autor" => "Dr. Frederic Llordachs Marqués",
+"resumen" => "es una de las aplicaciones en la nube más conocidas, y te permite guardar todos tus archivos y acceder a ellos desde cualquier dispositivo. Cuentas con una capacidad inicial de 2 gigas que puedes ir aumentando si invitas a otros amigos o pasas a un plan de pago.
+
+Para la gestión integral de tu clínica te recomendamos el uso de Clinic Cloud, nuestra aplicación en la nube que te permite entre otras muchas cosas la gestión del historial médico online. Puedes probar ya tu demo gratuita durante 15 días.",
 ];
 ?>
